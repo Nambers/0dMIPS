18b9c2114ad5cca0df56bb0e3226f3197594d124
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-12-28 17:42:47 -0600

    fix: fix memory write

--- src/modules/data_mem.sv
+++ src/modules/data_mem.sv
@@ -23,7 +23,7 @@ module data_mem (
     inst_addr
 );
     parameter  // size of data segment
-    data_start = 64'h10000000, data_words = 'h4000;  /* 4 M */
+    data_words = 'h4000;  /* 4 M */
 
     input clk, reset;
 
@@ -32,16 +32,14 @@ module data_mem (
     output [31:0] inst;
     /* verilator lint_off UNUSEDSIGNAL */
     input [63:0] inst_addr;
-    /* verilator lint_on UNUSEDSIGNAL */
     input [63:0] addr;  // Memory address
+    /* verilator lint_on UNUSEDSIGNAL */
     input [63:0] data_in;  // Memory write data
     input word_we;  // Write enable (active high)
     input byte_we;  // Write enable (active high)
 
-    wire           valid_address;
-
     // Memory segments
-    reg     [63:0] data_seg      [0:data_words-1] /* verilator public */;
+    reg     [63:0] data_seg[0:data_words-1]  /* verilator public */;
 
     // Verilog implementation stuff
     integer        i;
@@ -55,22 +53,25 @@ module data_mem (
         $readmemh("memory.data.mem", data_seg);
     end
 
-    assign valid_address = (addr >= data_start) && (addr < (data_start + data_words));
-
-    wire [13:0] index = addr[16:3];
+    wire [ 5:0] index = addr[8:3], inst_index = inst_addr[8:3];
     wire [63:0] d_out = data_seg[index];
     // TODO 32bit read
     assign data_out = d_out;
-    assign inst = (inst_addr[2] == 1'b0) ? data_seg[inst_addr[16:3]][31:0] : data_seg[inst_addr[16:3]][63:32];
+    assign inst = (inst_addr[2] == 1'b0) ? data_seg[inst_index][31:0] : data_seg[inst_index][63:32];
 
     always @(negedge clk or posedge reset) begin
         if (reset == 1'b1) begin
             for (i = 0; i < data_words; i = i + 1) data_seg[i] <= 64'b0;
             $readmemh("memory.text.mem", data_seg);
             $readmemh("memory.data.mem", data_seg);
-        end else if (valid_address == 1'b1) begin
-            if (word_we == 1'b1) data_seg[index] <= data_in;
-            else begin
+        end else begin
+            if (word_we == 1'b1) begin
+                if (addr[2] == 1'b0) begin
+                    data_seg[index][31:0] <= data_in[31:0];
+                end else begin
+                    data_seg[index][63:32] <= data_in[31:0];
+                end
+            end else begin
                 if (byte_we == 1'b1) begin
                     data_seg[index] <= (d_out & ~(64'hff << (8*(addr[2:0])))) | ((data_in & 64'hff) << (8*(addr[2:0])));
                 end
